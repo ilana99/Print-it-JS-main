99a73f8e2187c3b06582f3e9b91e82802177861e
ilana99 <132579012+ilana99@users.noreply.github.com>
2023-05-05 15:55:57 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,19 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
@@ -31,23 +31,23 @@ let paragraph = div.querySelector("p")
 let currentIndex = 0;
 
 function showSlide() {
-    banner_img.src = slides[currentIndex].image; 
+	banner_img.src = slides[currentIndex].image;
 }
 
 function changeDot() {
-	dot_selected.classList.remove('dot_selected');
-	const currentDot  = dots[currentIndex];
+	dot_selected.classList.remove('dot_selected'); // pour le dot_selected par défaut
+	const currentDot = dots[currentIndex];
 	currentDot.classList.add('dot_selected');
 	const prevIndex = (currentIndex - 1 + dots.length) % dots.length;
 	dots[prevIndex].classList.remove('dot_selected');
 }
 
-function changeDotRetour () {
+function changeDotRetour() {
 	dot_selected.classList.remove('dot_selected'); // pour le dot_selected par défaut
-	currentIndex = (currentIndex - 1 + slides.length) % slides.length; 
-	prevDot = dots[currentIndex];
+	currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+	const prevDot = dots[currentIndex];
 	prevDot.classList.add('dot_selected');
-	dotBefore = dots[currentIndex + 1];
+	const dotBefore = dots[currentIndex + 1];
 	dotBefore.classList.remove('dot_selected');
 }
 
@@ -55,17 +55,17 @@ function changeText() {
 	paragraph.innerHTML = slides[currentIndex].tagLine;
 }
 
-arrow_right.addEventListener("click", function() {
+arrow_right.addEventListener("click", function () {
 	currentIndex = (currentIndex + 1) % slides.length;
-    showSlide();
+	showSlide();
 	changeDot();
 	changeText();
 })
 
 
-arrow_left.addEventListener("click", function() {
-	 
-    showSlide(); 
+arrow_left.addEventListener("click", function () {
+
+	showSlide();
 	changeText();
 	changeDotRetour();
 })
